b5edb5f0388cd350de3ed47358b988522ed5ea8d
Lena Schimmel <mail@lenaschimmel.de>
2021-03-27 17:00:16 +0100

    Download full district data to differntiate "Landkreis" and "Stadtkreis".

--- tools/updateIncidence.sh
+++ tools/updateIncidence.sh
@@ -6,3 +6,4 @@ cd $BASEDIR
 curl https://api.corona-zahlen.org/germany/history/incidence/8 -o ../site/data/germany.json
 curl https://api.corona-zahlen.org/states/history/incidence/8 -o ../site/data/states.json
 curl https://api.corona-zahlen.org/districts/history/incidence/8 -o ../site/data/districts.json
+curl https://api.corona-zahlen.org/districts -o ../site/data/districts_full.json
