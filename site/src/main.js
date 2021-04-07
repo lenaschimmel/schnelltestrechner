b8bf727b76ee816b248d4552180af1ca55c7276a
Lena Schimmel <mail@lenaschimmel.de>
2021-04-07 16:23:26 +0200

    Add vue-screen

--- site/src/main.js
+++ site/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import VueScreen from 'vue-screen';
 
 Vue.config.productionTip = false
+Vue.use(VueScreen);
 
 new Vue({
   vuetify,
