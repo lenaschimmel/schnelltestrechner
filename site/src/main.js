0eb3da5780774d89960309ddd425ef90aa640500
Lena Schimmel <mail@lenaschimmel.de>
2021-04-17 02:56:47 +0200

    Use roboto-font from fontsource. Also uncommited babel plugin stuff?!

--- site/src/main.js
+++ site/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import VueScreen from 'vue-screen';
+import "@fontsource/roboto";
 
 Vue.config.productionTip = false
 Vue.use(VueScreen);
